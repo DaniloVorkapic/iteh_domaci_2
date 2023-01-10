51d2abbade5cb6b839effaf05d443811233e704f
DaniloVorkapic <vorkapic.danilo13@gmail.com>
2023-01-10 11:34:14 +0100

    autorizacija koriscenjem sanctuma

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\KupovinaController;
 use App\Http\Controllers\KursController;
 use App\Http\Controllers\PolazinkController;
@@ -16,18 +17,30 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/kupovine',[KupovinaController::class,'index']);
+
+Route::post('/register',[AuthController::class,'register']);
+Route::post('/login',[AuthController::class,'login']);
+
+
+
+
+
 
 Route::get('/kursevi',[KursController::class,'index']);
-
-
 Route::get('/polaznici',[PolazinkController::class,'index']);
 Route::get('/polaznici/{id}',[PolazinkController::class,'show']);
 
-Route::delete('/polaznici/{id}',[PolazinkController::class,'destroy']);
-Route::post('/polaznici',[PolazinkController::class,'store']);
-
-Route::put('/polaznici/{id}',[PolazinkController::class,'update']);
 
 
+
+Route::group(['middleware' => ['auth:sanctum']], function () {
+
+    Route::post('/logout',[AuthController::class,'logout']);
+
+    Route::get('/kupovine',[KupovinaController::class,'index']);
+    Route::delete('/polaznici/{id}',[PolazinkController::class,'destroy']);
+    Route::post('/polaznici',[PolazinkController::class,'store']);
+
+    Route::put('/polaznici/{id}',[PolazinkController::class,'update']);
+});
 
